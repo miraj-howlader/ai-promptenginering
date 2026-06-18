299f02b3f37c0463398c2731f25080e3c274d4e2
miraj <mirajhowlader9999@gmail.com>
2026-06-18 18:05:59 +0600

    setup betterauth

---
+++ src/app/login/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+
+        <div>
+            <h1 className='text-black mt-133'>Login</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/app/register/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className='mt-33'>Register</div>
+  )
+}
+
+export default Register
